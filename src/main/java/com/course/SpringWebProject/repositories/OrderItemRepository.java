41b4db682843d206201e47272cb493b92790adaa
Artur-Bertoni <artur_b_santos@hotmail.com>
2022-12-05 11:22:55 -0300

    Payment, one-to-one association

--- src/main/java/com/course/SpringWebProject/repositories/OrderItemRepository.java
+++ src/main/java/com/course/SpringWebProject/repositories/OrderItemRepository.java
@@ -1,9 +1,10 @@
 package com.course.SpringWebProject.repositories;
 
 import com.course.SpringWebProject.entities.OrderItem;
+import com.course.SpringWebProject.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 
 }
